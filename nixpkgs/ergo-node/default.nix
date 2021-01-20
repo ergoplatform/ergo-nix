4c63094bb8b652e20e662b750db70f3e8a8f8a14
Marek Mahut <marek.mahut@fivebinaries.com>
2021-01-20 14:37:39 +0100

    ergo-node: 4.0.0 -> 4.0.1

--- nixpkgs/ergo-node/default.nix
+++ nixpkgs/ergo-node/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "ergo-node";
-  version = "4.0.0";
+  version = "4.0.1";
 
   src = fetchurl {
     url = "https://github.com/ergoplatform/ergo/releases/download/v${version}/ergo-${version}.jar";
-    sha256 = "1dn6hj6wk2f5iivds586sqrwqqcapqm6dkfjb0wcvabizdvj0j9k";
+    sha256 = "0njiqp1srzq058spzv7j4zqpmf343wsm5gfxbird82c5n79ic3gb";
   };
 
   nativeBuildInputs = [ makeWrapper ];
